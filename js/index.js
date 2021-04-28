4d27443bd8516f455e1172dffb175a9200bcd1bf
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-04-28 16:17:39 +0600

    upload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 
-const API_URL = "http://localhost:3000/api/posts";
-const API_BASE_URL = "http://localhost:3000/";
+const API_URL = "http://localhost:5050/api/posts";
+const API_BASE_URL = "http://localhost:5050/";
 
 window.onload = () => {
     getPosts();
@@ -11,10 +11,25 @@ const getPosts = () => {
     fetch(API_URL, {
         method: "GET",
     })
-    .then(res => res.json())
-    .then(data => buildPosts(data))
+        .then(res => res.json())
+        .then(data => buildPosts(data))
 }
 
 const buildPosts = (blogPosts) => {
-    console.log(blogPosts)
+    let content = ''
+    for (blogPost of blogPosts) {
+        const postDate = new Date(parseInt(blogPost.added_date)).toDateString()
+        const postImage = `${API_BASE_URL}${blogPost.post_image}`
+        content += `
+                <div class="post">
+                    <div class="post-image" style='background-image: url(${postImage})'></div>
+                    <div class="post-content">
+                        <div class="post-date">${postDate}</div>
+                        <div class="post-title"><h4>${blogPost.title}</h4></div>
+                        <div class="post-text">${blogPost.content}</div>
+                    </div>
+                </div>
+        `
+    }
+    document.querySelector('.blog-post').innerHTML = content
 }
