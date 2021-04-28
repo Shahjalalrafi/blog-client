b4a4c94c381de207d1ed43c7e631a626a5564ad9
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-04-28 15:13:02 +0600

    upload

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+const API_URL = "http://localhost:3000/api/posts";
+const API_BASE_URL = "http://localhost:3000/";
+
+window.onload = () => {
+    getPosts();
+}
+
+const getPosts = () => {
+    fetch(API_URL, {
+        method: "GET",
+    })
+    .then(res => res.json())
+    .then(data => buildPosts(data))
+}
+
+const buildPosts = (blogPosts) => {
+    console.log(blogPosts)
+}
